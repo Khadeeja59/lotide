6c11ebb21630231fa567dc19cb860b8a95ba213d
Khadeeja59 <kkausar59@gmail.com>
2021-11-24 22:34:53 +0000

    Finding middle element in an array

---
+++ middle.js
@@ -0,0 +1,52 @@
+const eqArrays = function(arr1, arr2) {
+  let comp = [];
+  let res;
+  if( Array.isArray(arr1) && Array.isArray(arr2)) {
+    if (arr1.length === arr2.length){
+      for (let i = 0; i < arr1.length; i++) {
+        if(arr1[i]!==arr2[i]){
+          return false;
+        }
+      }
+    return true;
+    } 
+    else {
+      return false;
+    }
+  }
+};
+
+const assertArraysEqual = function(ar1, ar2) {
+  let val = eqArrays(ar1, ar2);
+  if (val === true)
+    console.log("Assertion Passed: [" + ar1 + "] === [" + ar2 + "]");
+  else if (val === false)
+    console.log("Assertion Failed: [" + ar1 + "] != [" + ar2 + "]");
+};
+
+
+const middle = function(arr){
+  let mid=[];
+  if (arr.length===1 || arr.length===2){
+    return [];
+  }
+  else if (arr.length %2 == 0 ){
+    let l;
+    l= arr.length/2;
+    mid=[arr[l-1],arr[l]];
+    return mid;
+  }
+  else if (arr.length % 2 !== 0) {
+    let l;
+    l = (arr.length-1)/2;
+    mid=[arr[l]];
+    return mid;
+
+  }
+
+};
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1,2,3,4]));
+console.log(middle([1,2,3,4,5,6]));
+assertArraysEqual(middle([1, 2, 3]), [2]);
