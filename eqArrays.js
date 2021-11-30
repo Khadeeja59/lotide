4461b649a7033d53230f62778c8c8824325cd7b5
Khadeeja59 <kkausar59@gmail.com>
2021-11-30 00:16:09 +0000

    Refactoring eqArrays for testing

--- eqArrays.js
+++ eqArrays.js
@@ -1,26 +1,55 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return true;
-  else
-    return false;
-};
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected)
+//     return true;
+//   else
+//     return false;
+// };
+
+
+
 const eqArrays = function(arr1, arr2) {
   let comp = [];
   let res;
-  for (let i = 0; i < arr1.length; i++)
-    comp.push(assertEqual(arr1[i], arr2[i]));
-  //console.log(comp);
-  for (let i = 0; i < comp.length; i++) {
-    if (comp[i] === false) {
-      res = false;
-    }
+  if( Array.isArray(arr1) && Array.isArray(arr2)) {
+    if (arr1.length === arr2.length){
+      for (let i = 0; i < arr1.length; i++) {
+        if(arr1[i]!==arr2[i]){
+          return false;
+        }
+      }
+    return true;
+    } 
     else {
-      res = true;
+      return false;
     }
   }
-  return res;
 };
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+
+
+
+
+
+
+
+// const eqArrays = function(arr1, arr2) {
+//   let comp = [];
+//   let res;
+//   for (let i = 0; i < arr1.length; i++)
+//     comp.push(assertEqual(arr1[i], arr2[i]));
+//   console.log(comp);
+//   for (let i = 0; i < comp.length; i++) {
+//     if (comp[i] === false) {
+//       res = false;
+//     }
+//     else {
+//       res = true;
+//     }
+//   }
+//   return res;
+// };
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+module.exports = eqArrays;  //added later
