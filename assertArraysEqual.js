598d59af1580cb0579c6c4e4c4a70735d8dfcbe7
Khadeeja59 <kkausar59@gmail.com>
2021-11-30 00:21:23 +0000

    Refactoring assertArraysEqual for testing

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,27 +1,29 @@
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return true;
-  else
-    return false;
-};
-const eqArrays = function(arr1, arr2) {
-  let comp = [];
-  let res;
-  for (let i = 0; i < arr1.length; i++)
-    comp.push(assertEqual(arr1[i], arr2[i]));
-  //console.log(comp);
-  for (let i = 0; i < comp.length; i++) {
-    if (comp[i] === false) {
-      res = false;
-    }
-    else {
-      res = true;
-    }
-  }
-  return res;
-  //console.log (res);
-};
+const eqArrays = require('./eqArrays');
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected)
+//     return true;
+//   else
+//     return false;
+// };
+// const eqArrays = function(arr1, arr2) {
+//   let comp = [];
+//   let res;
+//   for (let i = 0; i < arr1.length; i++)
+//     comp.push(assertEqual(arr1[i], arr2[i]));
+//   //console.log(comp);
+//   for (let i = 0; i < comp.length; i++) {
+//     if (comp[i] === false) {
+//       res = false;
+//     }
+//     else {
+//       res = true;
+//     }
+//   }
+//   return res;
+//   //console.log (res);
+// };
 //console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
 //console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 //console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));// => true
@@ -34,7 +36,8 @@ const assertArraysEqual = function(ar1, ar2) {
   else if (val === false)
     console.log("Assertion Failed: [" + ar1 + "] != [" + ar2 + "]");
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", 2, 3], [3, 2, 1]);
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
+// assertArraysEqual([1, 2, 3], [3, 2, 1]);
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+// assertArraysEqual(["1", 2, 3], [3, 2, 1]);
+module.exports = assertArraysEqual;
