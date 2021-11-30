d67f5fd6f1d328995b348aebcde39e7c8381935c
Khadeeja59 <kkausar59@gmail.com>
2021-11-30 00:09:36 +0000

    Refactoring tail.js for testing

--- tail.js
+++ tail.js
@@ -1,26 +1,28 @@
-const assertEqual = function(actual, expected) {
-  if (actual === " ")
-    console.log("Undefined");
-  else {
-    if (actual === expected)
-      console.log("Assertion Passed:" + actual + "===" + expected);
-    else if (actual !== expected)
-      console.log("Assertion Failed:" + actual + "!=" + expected);
-  }
-};
+// const assertEqual = function(actual, expected) {    (commented to create tailTest)
+//   if (actual === " ")
+//     console.log("Undefined");
+//   else {
+//     if (actual === expected)
+//       console.log("Assertion Passed:" + actual + "===" + expected);
+//     else if (actual !== expected)
+//       console.log("Assertion Failed:" + actual + "!=" + expected);
+//   }
+// };
 
 const tail = function(tail) {
   let theTail = tail.splice(1);
-  console.log(theTail);
+  //console.log(theTail);
   return theTail;
 };
+module.exports = tail;  //added later
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-let newTail = tail(words);
-console.log(newTail); // no need to capture the return value since we are not checking it
-assertEqual(newTail.length, 3); // original array should still have 3 elements!
-assertEqual(newTail.length, 1); // original array should still have 3 elements!
-assertEqual(newTail[0],'Lighthouse');
-assertEqual(newTail[1],'Lighthouse');
+//(commented to create tailTest)
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// let newTail = tail(words);
+// console.log(newTail); // no need to capture the return value since we are not checking it
+// assertEqual(newTail.length, 3); // original array should still have 3 elements!
+// assertEqual(newTail.length, 1); // original array should still have 3 elements!
+// assertEqual(newTail[0],'Lighthouse');
+// assertEqual(newTail[1],'Lighthouse');
 
 
